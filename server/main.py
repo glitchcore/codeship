38d4b010f90633294b78bccdfa76c1376509aa6d
aanper <mail@s3f.ru>
2022-01-10 13:54:11 +0300

    websocket example

--- server/main.py
+++ server/main.py
@@ -1,15 +1,17 @@
-from time import sleep
+#!/usr/bin/env python
 
-my_code = '''
-x = 10
-print(y)
-print(x)
-'''
+import asyncio
+import datetime
+import random
+import websockets
 
-y = 2
-for line in my_code.splitlines():
-	print(f"line: {line}")
-	exec(line)
-	sleep(1)
+async def get_time(websocket, path):
+    while True:
+        now = datetime.datetime.utcnow().isoformat() + 'Z'
+        await websocket.send(now)
+        await asyncio.sleep(random.random() * 3)
 
-print(x)
+start_server = websockets.serve(get_time, '0.0.0.0', 5678)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
